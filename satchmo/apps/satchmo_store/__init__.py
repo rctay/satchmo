75e494275dd39be0f2eb87ea3b7d6867fb8f5d01
Johannes Bornhold <joh@bo-tech.de>
2010-11-28 21:42:59 +0100

    Changed version numbering scheme to X.Y.Z instead of X.Y-Z
    This will make "satchmo_store.__version__" more consistent with
    "satchmo_store.get_version()" and avoid possible irritations.

--- satchmo/apps/satchmo_store/__init__.py
+++ satchmo/apps/satchmo_store/__init__.py
@@ -2,7 +2,7 @@ VERSION = (0, 9, 2, 'pre')
             
 # Dynamically calculate the version based on VERSION tuple
 if len(VERSION)>2 and VERSION[2] is not None:
-    str_version = "%d.%d_%s" % VERSION[:3]
+    str_version = "%d.%d.%s" % VERSION[:3]
 else:
     str_version = "%d.%d" % VERSION[:2]
 
